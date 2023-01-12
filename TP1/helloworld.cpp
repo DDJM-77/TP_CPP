7e9bdcfff773dc5070107bd3e8122f8af280317f
DDJM-77 <ddjm77340@gmail.com>
2023-01-12 16:36:45 +0100

    Update of the helloworld file.

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
-    std::cout << "Hello World !" << std::endl;
+    int age = 0;
+    std::cout << "Hello World !" << std::endl
+              << "How old are you ?" << std::endl;
+    std::cin >> age;
+    std::cout << "You are " << age << " years old" << std::endl;
     return 0;
 }
