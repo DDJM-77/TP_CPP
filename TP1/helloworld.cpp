86b73ccc96ec11af3ceca4b446fe00241b2a58be
DDJM-77 <ddjm77340@gmail.com>
2023-01-12 16:50:05 +0100

    Compiler options to have the auto word and string manipulation.

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -2,10 +2,16 @@
 
 int main()
 {
-    int age = 0;
+    auto age = std::string{};
+    auto name = std::string{};
     std::cout << "Hello World !" << std::endl
               << "How old are you ?" << std::endl;
     std::cin >> age;
     std::cout << "You are " << age << " years old" << std::endl;
+    std::cout << "What's your name ?" << std::endl;
+    std::cin >> name;
+    std::cout << "Hello " << name << std::endl;
     return 0;
 }
+
+// Les chaînes de caractères en C++ sont représentées par std::string
