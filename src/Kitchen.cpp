6bc790d1759fd58aaad8ccd8a28769c6a77197a9
DDJM-77 <ddjm77340@gmail.com>
2023-03-28 14:45:38 +0200

    Here

--- src/Kitchen.cpp
+++ src/Kitchen.cpp
@@ -1,1 +1,21 @@
 #include "Kitchen.hpp"
+
+#include <memory>
+
+Unit& Kitchen::register_unit(Unit new_unit)
+{
+    _all_units.emplace_back(new_unit);
+    return _all_units.back();
+}
+
+Unit* Kitchen::find_unit(const std::string& str)
+{
+    auto it =
+        std::find_if(_all_units.begin(), _all_units.end(), [str](Unit str_1) { return str_1.name == str; });
+    if (it != _all_units.end())
+    {
+        return &(it->get());
+    }
+
+    return nullptr;
+}
--- src/Kitchen.hpp
+++ src/Kitchen.hpp
@@ -1,4 +1,16 @@
 #pragma once
 
+#include "../lib/Unit.hpp"
+
+#include <algorithm>
+#include <vector>
+
 class Kitchen
-{};
+{
+public:
+    Unit& register_unit(Unit new_unit);
+    Unit* find_unit(const std::string& str);
+
+private:
+    std::vector<std::reference_wrapper<Unit>> _all_units;
+};
