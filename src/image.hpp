8bf4225af5fc2960d96da2d452ec9a29f6717621
DDJM-77 <ddjm77340@gmail.com>
2023-04-04 14:29:20 +0200

    Commit

--- src/image.hpp
+++ src/image.hpp
@@ -1,13 +1,38 @@
 #pragma once
 
+#include "../lib/lib.hpp"
 #include "pixels.hpp"
 
-#include "../lib/lib.hpp"
-
-#include <stdint.h>
+#include <array>
 #include <cstddef>
 #include <functional>
-#include <string>
-#include <array>
 #include <iostream>
+#include <stdint.h>
+#include <string>
 
+template <typename P, size_t W, size_t H>
+class Image
+{
+public:
+    Image() {}
+
+    Image(const P& pixel)
+        : _pixel { pixel }
+    {
+        for (int i = 0; i < H; i++)
+        {
+            for (int j = 0; j < W; j++)
+            {
+                _pixels[i][j] = _pixel;
+            }
+        }
+    }
+
+    P& operator()(const size_t i, const size_t j) { return _pixels[j][i]; }
+
+    const P& operator()(const size_t i, const size_t j) const { return _pixels[j][i]; }
+
+private:
+    P                               _pixel;
+    std::array<std::array<P, W>, H> _pixels;
+};
--- tests/Tests-0102-image-accessor.cpp
+++ tests/Tests-0102-image-accessor.cpp
@@ -1,26 +1,25 @@
-#include "../src/pixels.hpp"
 #include "../src/image.hpp"
+#include "../src/pixels.hpp"
 
 #include <catch2/catch_test_macros.hpp>
-
+#include <complex>
 #include <iostream>
 #include <type_traits>
-#include <complex>
 
 TEST_CASE("Image pixel access with image(i,j)")
 {
     Image<int, 4, 4> img(0);
-    img(1,2) = 2;
-    img(2,1) = 5;
-    
-    REQUIRE(img(1,2) == 2);
-    REQUIRE(img(2,1) == 5);
+    img(1, 2) = 2;
+    img(2, 1) = 5;
+
+    REQUIRE(img(1, 2) == 2);
+    REQUIRE(img(2, 1) == 5);
 }
 
 TEST_CASE("Same thing but const")
 {
     const Image<int, 4, 4> img(3);
-    
-    REQUIRE(img(1,2) == 3);
-    REQUIRE(img(2,1) == 3);
+
+    REQUIRE(img(1, 2) == 3);
+    REQUIRE(img(2, 1) == 3);
 }
