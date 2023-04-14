d9ba5a956c4bcaa26bb808ab570fcbcbbe9f8aea
Dylan DE JESUS <dejesusmilitar@edu.univ-eiffel.fr>
2023-04-14 10:20:34 +0200

    Update

--- src/ex1.cpp
+++ src/ex1.cpp
@@ -1,1 +1,14 @@
 #include "ex1.hpp"
+
+void pairwise_concatenate(std::list<std::list<int>>& l1, std::list<std::list<int>> l2)
+{
+    auto it1 = l1.begin();
+    auto it2 = l2.begin();
+
+    while (it1 != l1.end() && it2 != l2.end())
+    {
+        concatenate(*it1, std::move(*it2));
+        it1++;
+        it2++;
+    }
+}
--- src/ex1.hpp
+++ src/ex1.hpp
@@ -1,1 +1,7 @@
 #pragma once
+#include "../lib/concatenate.hpp"
+
+#include <list>
+#include <memory>
+
+void pairwise_concatenate(std::list<std::list<int>>& l1, std::list<std::list<int>> l2);
